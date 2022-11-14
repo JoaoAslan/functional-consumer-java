8d8a49ca55222c1324f3908c2119004727cf6e86
JoaoAslan <contato.joaoaslan@gmail.com>
2022-11-13 21:35:34 -0300

    Solution with Reference Method (non-static)

--- application/Program.java
+++ application/Program.java
@@ -16,7 +16,7 @@ public class Program {
         list.add(new Product("Tablet", 350.5));
         list.add(new Product("HD Case", 80.9));
 
-        list.forEach(Product::staticPriceUpdate);
+        list.forEach(Product::nonStaticPriceUpdate);
 
         list.forEach(System.out::println);
     }
